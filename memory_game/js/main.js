fde63cf6e9c99b77a800ff6aec6b99328a9c0e70
Wesley Hall <hallwesley@outlook.com>
2019-01-20 12:44:48 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,26 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
 var cardsInPlay = [];
 
 var checkForMatch = function() {
@@ -12,8 +34,9 @@ var checkForMatch = function() {
 }
 
 var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId])
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank)
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
 	checkForMatch();
 }
 
