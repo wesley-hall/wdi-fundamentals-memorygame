499980e530965f32e4b24b1dc498e30894dd8a8f
Wesley Hall <hallwesley@outlook.com>
2019-01-20 18:25:04 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -33,12 +33,27 @@ var checkForMatch = function() {
 	}
 }
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id')
 	console.log("User flipped " + cards[cardId].rank)
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
-	checkForMatch();
-}
+	this.setAttribute('src', cards[cardId].cardImage);
+	// setTimeout is used to delay the checkForMatch function - so that there is a delay between the card image changing and the alert being displayed
+	setTimeout(function() {
+		checkForMatch();
+	}, 100);
+};
 
-flipCard(0);
-flipCard(2);
+
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	};
+};
+
+createBoard();
